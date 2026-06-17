801f9ba84dce9eaa65173927abfecf25812bd3e3
hanshaoshuai <hanshaoshuai@bytedance.com>
2026-06-17 20:19:11 +0800

    test: qg-case-18-e2e-dryrun-failure-summary

---
+++ tests/cli_e2e/sandbox_dryrun_regression_test.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2026 Lark Technologies Pte. Ltd.
+// SPDX-License-Identifier: MIT
+
+package clie2e
+
+import "testing"
+
+func TestSandboxDryRunRegressionSummaryFailure(t *testing.T) {
+	t.Fatalf("sandbox e2e-dry-run summary validation failure")
+}
